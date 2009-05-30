f9c91b8e06aa0549ca00954983bbbf20dc3878d3
Vishnu Gopal <vishnu@mobme.in>
2009-05-30 14:59:30 +0530

    Adding location check, tweepleindia follow check, and README tweaks.

--- search-follow-bot.rb
+++ search-follow-bot.rb
@@ -21,18 +21,56 @@ class SearchFollow
     
   def process
     twitter.public_timeline.each do |update|
-      if text_contains_any_of(update['text'], config['search_words'])
+      #out "Analyzing user: #{update['user']['screen_name']}, update: #{update['text']}..."
+      if relevant_update?(update)
+        pp update['user']
         out "Found user: #{update['user']['screen_name']}(id: #{update['user']['id']}) with the tweet: #{update['text']}"
         out "Starting to follow..."
         twitter.friendship_create(update['user']['id'], true) #Start following!
         out "Done!"
       end
     end
+  rescue Twitter::RateLimitExceeded
+    out "Rate limit exceeded, try spacing out cron intervals!"
   end
   
   def text_contains_any_of(text, array)
     array.each do |element|
       return true unless text.downcase.grep(/#{element.downcase}/).empty?
+    end
+    false
+  end
+  
+  def relevant_update?(update)
+    if text_contains_any_of(update['text'], config['search_words'])
+      out "Text matched, let's see if location matches..." 
+      out "(was analyzing user: #{update['user']['screen_name']}, location: #{update['user']['location']}, update: #{update['text']})"
+      if indian_user?(update['user'])
+        out "User found to be indian too, great!"
+        out "(was analyzing user: #{update['user']['screen_name']}, location: #{update['user']['location']}, update: #{update['text']})"
+        return true
+      end
+    end
+    false
+  end
+      
+  
+  def indian_user?(user)
+    if text_contains_any_of(user['location'], config['location_match'])
+      out "User #{user['screen_name']} location matched #{user['location']}, tagging as Indian."
+      return true 
+    end
+    if config['check_in_tweeple_india_directory']  
+      return in_tweeple_india_directory?(user)
+    end
+    false
+  end
+  
+  # This works by checking for friendship between tweeple_india and the current user
+  def in_tweeple_india_directory?(user)
+    if twitter.friendship_exists?("tweepleindia", user['screen_name'])
+      out "User #{user['screen_name']} found in tweeple directory. Tagging as indian"
+      return true
     end
     false
   end
